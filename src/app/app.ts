63aa91d6dcfb59a71fd9d22bcb96a82b65b91403
Ruma303 <matteoh8@hotmail.it>
2025-07-15 09:59:32 +0200

    signal output ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormComponent } from './components/form/form';
-import { FormValidatedComponent } from './components/form-validated/form-validated';
-import { NgModelGroupComponent } from './components/ng-model-group-component/ng-model-group-component';
 
 @Component({
   selector: 'app-root',
-  imports: [FormComponent, FormValidatedComponent, NgModelGroupComponent],
-  templateUrl: './app.html',
+  imports: [FormComponent],
+  template: `
+  <h1>{{ title }}</h1>
+  <h3>{{ welcomeMessage() }}</h3>
+  <app-form (formExported)="handleForm($event)"></app-form>
+  `,
   styles: ``
 })
 export class App {
-  protected title = 'angular-forms';
+  title: string = 'angular-forms';
+  welcomeMessage = signal<string>('Welcome to Angular Forms!');
+
+  handleForm(form: { username: string; email: string; age: number }) {
+    console.log('Form submitted:', form);
+    this.welcomeMessage.set(`Welcome, ${form.username}!`);
+  }
 }
--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, effect, output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { WritableSignal, Signal } from '@angular/core';
 
+interface form {
+  username: string;
+  email: string;
+  age: number;
+}
 @Component({
   selector: 'app-form',
+  standalone: true,
   imports: [FormsModule, CommonModule],
-  templateUrl: './form.html',
+  template: `
+    <h2>Template-driven forms</h2>
+
+    <form #myForm="ngForm" (ngSubmit)="submitForm(myForm)" class="form">
+      <label for="username">
+        Username: <span>{{ username() }}</span>
+      </label>
+      <input [(ngModel)]="usernameModel" name="username">
+
+      <label for="email">
+        Email: <span>{{ email() }}</span>
+      </label>
+      <input [(ngModel)]="emailModel" name="email">
+
+      <label for="age">
+        Age: <span>{{ age() }}</span>
+      </label>
+      <input [(ngModel)]="ageModel" name="age" type="number">
+
+      <button type="submit" [disabled]="myForm.invalid">Invia</button>
+    </form>
+
+    @if(submitted()) {
+      <div>
+        <h3>Form submitted</h3>
+        <p>Username: {{ form()?.username }}</p>
+        <p>Email: {{ form()?.email }}</p>
+        <p *ngIf="isAdult()">Age: {{ form()?.age }}</p>
+      </div>
+    }
+    @if(visibleError()) {
+      <div>
+        <h3>{{ visibleError() }}</h3>
+      </div>
+    }
+  `,
   styles: `
     .form {
       display: flex;
@@ -15,18 +57,56 @@ import { CommonModule } from '@angular/common';
   `
 })
 export class FormComponent {
-  username: string = '';
-  email : string = '';
-  submitted: boolean = false;
-  form: any = {};
+  usernameModel: string = '';
+  emailModel: string = '';
+  ageModel: number = 0;
+
+  username: WritableSignal<string> = signal('');
+  email: WritableSignal<string> = signal('');
+  age: WritableSignal<number> = signal(0);
+  errorMessage = signal<string>('');
+
+  submitted: WritableSignal<boolean> = signal(false);
+  isAdult: Signal<boolean> = computed(() => this.age() !== null && this.age()! >= 18);
+
+  readonly visibleError = computed(() => !this.submitted() && this.errorMessage());
+
+  form: WritableSignal<form | null> = signal(null);
+
+  formExported = output<form>();
 
   constructor() {
-    console.log('FormComponent initialized');
+    effect(() => {
+      this.username.set(this.usernameModel);
+      this.email.set(this.emailModel);
+    });
   }
 
-  submitForm(value: NgForm) {
-    console.log('Form submitted with value:', value);
-    this.submitted = true;
-    this.form = value.value;
+  submitForm(form: NgForm) {
+    // sincronizza i dati del modello prima di validare
+    this.username.set(this.usernameModel);
+    this.email.set(this.emailModel);
+    this.age.set(this.ageModel);
+
+    if (!this.isAdult()) {
+      this.submitted.set(false);
+      return this.errorMessage.set('You must be at least 18 years old to submit the form.');
+    }
+
+    if (form.valid) {
+      const payload = {
+        username: this.username(),
+        email: this.email(),
+        age: this.age()
+      };
+      this.form.set(payload);
+      this.submitted.set(true);
+      this.errorMessage.set('');
+      this.formExported.emit(payload); // emit the form data
+    } else {
+      this.submitted.set(false);
+      this.errorMessage.set('Please fill out all fields correctly.');
+    }
   }
 }
+
